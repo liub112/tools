b3c4666b6c582c4fedaa2ca14fc4ed4696b9ec61
liub_112 <liub_112@foxmail.com>
2018-07-09 19:29:56 +0800

    add Mysql-schamle tables

--- tools-core/src/main/java/com/lfxfs/tools/core/metaData/MySqlExtractTableMetaData.java
+++ tools-core/src/main/java/com/lfxfs/tools/core/metaData/MySqlExtractTableMetaData.java
@@ -8,28 +8,62 @@ import com.lfxfs.tools.core.model.Table;
 import javax.swing.*;
 import java.sql.*;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class MySqlExtractTableMetaData implements ExtractTableMetaData{
     @Override
     public List<Table> extractTableMetaData(DataSource dataSource) {
         List<Table> lists = new ArrayList<Table>();
         Connection conn = null;
-        String sql ="select * from dba_tables order by create_date desc";
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String sql ="SELECT A.ordinal_position, \n" +
+                "\tB.TABLE_NAME,\n" +
+                "\tb.table_COMMENT,\n" +
+                "\tA.COLUMN_NAME,\n" +
+                "\tA.DATA_TYPE,\n" +
+                "\tA.COLUMN_COMMENT\n" +
+                "FROM\n" +
+                "\tinformation_schema.COLUMNS a,\n" +
+                "\tinformation_schema.TABLES b \n" +
+                "WHERE\n" +
+                "\ta.TABLE_SCHEMA = b.TABLE_SCHEMA \n" +
+                "\tAND A.TABLE_NAME = B.TABLE_NAME\n" +
+                "\tAND a.table_schema = 'apolloconfigdb' \n" +
+                "\tAND B.table_type = 'base table' \n" +
+                "\tAND A.table_name = 'appnamespace' \n" +
+                "ORDER BY\n" +
+                "\tA.ordinal_position";
         try {
             conn = dataSource.getConnection();
             PreparedStatement prtm = conn.prepareStatement(sql);
             ResultSet rs = prtm.executeQuery();
+            Set<Table> tableSet = new HashSet<Table>();
+            List<Field> fieldList = new ArrayList<Field>();
             while(rs.next()){
-                String tableName=	rs.getString(1);
-                String comments = rs.getString(2);
-                Table table = new Table(tableName,comments);
-                List<Field> fields = queryTableFiedByTableName(conn,tableName);
-                table.setFields(fields);
-                lists.add(table);
+                String tableName=	rs.getString(2);
+                String tablecomments = rs.getString(3);
+                Table table = new Table(tableName,tablecomments);
+                tableSet.add(table);
+
+                String columnName = rs.getString(4);
+                String dataType = rs.getString(5);
+                String columncomments = rs.getString(6);
+                Field field = new Field(tableName,columnName,columncomments,dataType);
+                fieldList.add(field);
             }
+            Iterator<Table> it = tableSet.iterator();
+            while (it.hasNext()) {
+                Table t = it.next();
+                List<Field> fields = new ArrayList<>();
+                for (int i = 0; i <fieldList.size() ; i++) {
+                    Field field = fieldList.get(i);
+                    if(field.getTableName().equals(t.getTableName())){
+                        fields.add(field);
+                    }
+                }
+                t.setFields(fields);
+                lists.add(t);
+            }
+
             rs.close();
             prtm.close();
         } catch (Exception e) {
